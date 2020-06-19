04e26982ba81e8f8848e54246bb759fd4e2344e5
Apon77 <61594531+Apon77@users.noreply.github.com>
2020-06-19 21:08:32 +0600

    case

--- install.sh
+++ install.sh
@@ -1,4 +1,17 @@
 #! /bin/bash
 #loadkeys dvorak
-ip a
+echo "select the operation ************"
+echo "  1)operation 1"
+echo "  2)operation 2"
+echo "  3)operation 3"
+echo "  4)operation 4" 
+
+read n
+case $n in
+  1) echo "You chose Option 1";;
+  2) echo "You chose Option 2";;
+  3) echo "You chose Option 3";;
+  4) echo "You chose Option 4";;
+  *) echo "invalid option";;
+esac
 
