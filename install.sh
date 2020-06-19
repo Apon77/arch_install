b71b8b985f350c240439b35a07c6af00f986f7eb
Apon77 <61594531+Apon77@users.noreply.github.com>
2020-06-19 20:53:36 +0600

    Testing dialog

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
-loadkeys dvorak
-ip a
+#loadkeys dvorak
+#ip a
+dialog --clear --backtitle "Backtitle here" --title "Title here" --menu "Choose one of the following options:" 15 40 4 \
+1 "Option 1" \
+2 "Option 2" \
+3 "Option 3"
