2d6b35444000122e9b404b6b9ef9c9e475169c89
Apon77 <61594531+Apon77@users.noreply.github.com>
2020-06-19 21:10:46 +0600

    ls

--- install.sh
+++ install.sh
@@ -1,17 +1,3 @@
 #! /bin/bash
 #loadkeys dvorak
-echo "select the operation ************"
-echo "  1)operation 1"
-echo "  2)operation 2"
-echo "  3)operation 3"
-echo "  4)operation 4" 
-
-read n
-case $n in
-  1) echo "You chose Option 1";;
-  2) echo "You chose Option 2";;
-  3) echo "You chose Option 3";;
-  4) echo "You chose Option 4";;
-  *) echo "invalid option";;
-esac
-
+ls
