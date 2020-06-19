42b7b636d50317c2651af1f4177a0e7eb6e22cd4
Apon77 <61594531+Apon77@users.noreply.github.com>
2020-06-19 21:01:40 +0600

    testing case

--- install.sh
+++ install.sh
@@ -1,7 +1,27 @@
 #! /bin/bash
 #loadkeys dvorak
 #ip a
-dialog --clear --backtitle "Backtitle here" --title "Title here" --menu "Choose one of the following options:" 15 40 4 \
-1 "Option 1" \
-2 "Option 2" \
-3 "Option 3"
+#dialog --clear --backtitle "Backtitle here" --title "Title here" --menu "Choose one of the following options:" 15 40 4 \
+#1 "Option 1" \
+#2 "Option 2" \
+#3 "Option 3"
+PS3='Please enter your choice: '
+options=("Option 1" "Option 2" "Option 3" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1")
+            echo "you chose choice 1"
+            ;;
+        "Option 2")
+            echo "you chose choice 2"
+            ;;
+        "Option 3")
+            echo "you chose choice $REPLY which is $opt"
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
